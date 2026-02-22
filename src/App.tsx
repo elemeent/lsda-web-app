aee9e9b234bf044b0152342ce0b00b08459180b3
Element <element.gtaw@gmail.com>
2026-02-21 21:38:31 -0300

    :adhesive_bandage: Fixing base app URL @ React Router

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <Router>
+    <Router basename="/lsda-web-app/">
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
